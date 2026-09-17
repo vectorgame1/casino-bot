dd447f7f3d2809a91f5a6bebb7f249aa46243c5d
vectorgame1 <bobrovnikivan9@gmail.com>
2026-09-17 20:03:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2329,7 +2329,7 @@ async def callback_handler(call: CallbackQuery):
         if win:
                 wa = clamp(int(bet * mult * get_event_mult() * get_user_mult(user_id)))
                 nb = set_balance(user_id, wa)
-            update_quest(user_id, "win_100k", wa)
+                update_quest(user_id, "win_100k", wa)
         if result == 36:
                 unlock_achievement(user_id, "lucky_36")
     txt = f"Win +{wa} (x{mult}) Balance: {nb}"
